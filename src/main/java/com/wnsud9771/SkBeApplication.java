0c4625d2306cf8e1c6dedc91de308f3f8d0cfb23
jun9771 <jun9771@gmail.com>
2025-01-03 06:43:46 +0900

    first

---
+++ src/main/java/com/wnsud9771/SkBeApplication.java
@@ -0,0 +1,13 @@
+package com.wnsud9771;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkBeApplication.class, args);
+	}
+
+}
