eb6eebba497b45ce1dacc9777e674da1c50f241e
jun9771 <jun9771@gmail.com>
2025-01-03 06:52:11 +0900

    base-structure

---
+++ src/main/java/com/wnsud9771/entity/SampleEntity.java
@@ -0,0 +1,12 @@
+//package com.wnsud9771.entity;
+//
+//import jakarta.persistence.Entity;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Entity
+//public class SampleEntity {
+//
+//}
---
+++ src/main/java/com/wnsud9771/repository/SampleRepository.java
@@ -0,0 +1,7 @@
+//package com.wnsud9771.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public class SampleRepository extends JpaRepository<entity, Long>{
+//
+//}
